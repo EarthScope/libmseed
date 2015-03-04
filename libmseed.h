06772ce8c7c1cd5e4a30dada628ffa46ca7fb403
Chad Trabant <chad@iris.washington.edu>
2015-03-04 00:46:12 +0000

    fix memory leak when msr_pack() returns after errors

--- libmseed.h
+++ libmseed.h
@@ -31,7 +31,7 @@ extern "C" {
 #include "lmplatform.h"
 
 #define LIBMSEED_VERSION "2.14"
-#define LIBMSEED_RELEASE "2015.061"
+#define LIBMSEED_RELEASE "2015.062"
 
 #define MINRECLEN   128      /* Minimum Mini-SEED record length, 2^7 bytes */
                              /* Note: the SEED specification minimum is 256 */
