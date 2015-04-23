6c7b76431f8e07e8caf811aad86a383b3d30f27c
Chad Trabant <chad@iris.washington.edu>
2015-04-23 21:13:35 +0000

    Update minor release version in Makefile

--- libmseed.h
+++ libmseed.h
@@ -31,7 +31,7 @@ extern "C" {
 #include "lmplatform.h"
 
 #define LIBMSEED_VERSION "2.16"
-#define LIBMSEED_RELEASE "2015.108"
+#define LIBMSEED_RELEASE "2015.113"
 
 #define MINRECLEN   128      /* Minimum Mini-SEED record length, 2^7 bytes */
                              /* Note: the SEED specification minimum is 256 */
