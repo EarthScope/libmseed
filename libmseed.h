5c40c00008204e3d84d7f48b937ee5220d32a097
chad-iris <chad.trabant@iris.edu>
2016-10-01 20:43:07 -0700

    Update version and release date

--- libmseed.h
+++ libmseed.h
@@ -32,8 +32,8 @@ extern "C" {
 
 #include "lmplatform.h"
 
-#define LIBMSEED_VERSION "2.18rc2"
-#define LIBMSEED_RELEASE "2016.274"
+#define LIBMSEED_VERSION "2.18rc3"
+#define LIBMSEED_RELEASE "2016.275"
 
 #define MINRECLEN   128      /* Minimum Mini-SEED record length, 2^7 bytes */
                              /* Note: the SEED specification minimum is 256 */
