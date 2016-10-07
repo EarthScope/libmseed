731b6f322a5dc16855a121aba853d92930aefcbc
Chad Trabant <chad@iris.washington.edu>
2016-10-07 16:15:18 -0700

    Remove some wayward license notices for consistency

--- lmplatform.c
+++ lmplatform.c
@@ -3,20 +3,6 @@
  * 
  * Platform portability routines.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public License
- * as published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License (GNU-LGPL) for more details.  The
- * GNU-LGPL and further information can be found here:
- * http://www.gnu.org/
- *
- * Written by Chad Trabant, IRIS Data Management Center
- *
  * modified: 2010.304
  ***************************************************************************/
 
