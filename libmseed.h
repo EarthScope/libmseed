55478018def5776bb63f7216577743042dfdfa1d
chad-iris <chad.trabant@iris.edu>
2018-08-28 23:20:52 -0700

    Change version to 2.19.6 and update ChangeLog

--- libmseed.h
+++ libmseed.h
@@ -28,8 +28,8 @@
 extern "C" {
 #endif
 
-#define LIBMSEED_VERSION "2.19.5"
-#define LIBMSEED_RELEASE "2017.283"
+#define LIBMSEED_VERSION "2.19.6"
+#define LIBMSEED_RELEASE "2018.240"
 
 /* C99 standard headers */
 #include <stdlib.h>
