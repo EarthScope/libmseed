31ba68f4f173f2ce25efeb39dd7805c6b3db84ac
chad-earthscope <chad.trabant@earthscope.org>
2023-05-13 10:53:43 -0700

    Add recptr tests

--- test/test_tracelist.c
+++ test/test_tracelist.c
@@ -4,7 +4,7 @@
 static MunitResult
 read_tests(const MunitParameter params[], void* data) {
   MS3TraceList *mstl = NULL;
-  MS3TraceID *id = NULL;
+  MS3TraceID *id     = NULL;
   nstime_t starttime;
   nstime_t endtime;
   uint32_t flags = 0;
@@ -40,6 +40,7 @@ read_tests(const MunitParameter params[], void* data) {
   munit_assert_int64 (id->first->numsamples, ==, 3952);
   munit_assert_null (id->next[0]);
   munit_assert_null (id->first->next);
+  munit_assert_ptr (id->first, ==, id->last);
 
   mstl3_free (&mstl, 0);
 
@@ -47,32 +48,151 @@ read_tests(const MunitParameter params[], void* data) {
 }
 
 static MunitResult
-recptr_tests (const MunitParameter params[], void *data)
+recptr_file_tests (const MunitParameter params[], void *data)
 {
-  MS3Record *msr = NULL;
-  MS3TraceList *mstl = NULL;
-  MS3TraceSeg *seg = NULL;
+  MS3TraceList *mstl   = NULL;
+  MS3TraceID *id       = NULL;
   MS3RecordPtr *recptr = NULL;
-  nstime_t starttime;
   nstime_t endtime;
+  int64_t unpacked;
   uint32_t flags = 0;
+  int32_t *int32s;
   int rv;
 
-  char *path = "data/test-encoding-steim2-alldifferences-LE.mseed2";
+  char *path = "data/test-oneseries-mixedlengths-mixedorder.mseed2";
 
   /* Silence compiler warnings about unused parameters */
   (void) params;
   (void) data;
 
-  /* General parsing */
-  while ((rv = ms3_readmsr (&msr, path, NULL, NULL, flags, 0)) == MS_NOERROR)
-  {
+  endtime = ms_timestr2nstime ("2010-02-27T07:55:51.069539Z");
 
-  }
+  /* Set bit flag to build a record list */
+  flags |= MSF_RECORDLIST;
 
-  //if ()
+  rv = ms3_readtracelist (&mstl, path, NULL, 0, flags, 0);
 
+  munit_assert_int (rv, ==, MS_NOERROR);
+  munit_assert_not_null (mstl);
+  munit_assert_uint32 (mstl->numtraceids, ==, 1);
 
+  id = mstl->traces.next[0];
+
+  munit_assert_not_null (id->first);
+  munit_assert_not_null (id->first->recordlist);
+  munit_assert_int64 (id->first->samplecnt, ==, 3952);
+
+  /* No data has been decoded */
+  munit_assert_char (id->first->sampletype, ==, 0);
+  munit_assert_null (id->first->datasamples);
+  munit_assert_int64 (id->first->numsamples, ==, 0);
+
+  recptr = id->first->recordlist->last;
+  munit_assert_not_null (recptr->filename); /* Record is in a file */
+  munit_assert_null (recptr->bufferptr);    /* Record is not in a buffer */
+  munit_assert_null (recptr->fileptr);      /* File is not currently open, closed by read routine */
+  munit_assert_int64 (recptr->fileoffset, ==, 1152);
+  munit_assert_not_null (recptr->msr);
+  munit_assert_int64 (recptr->endtime, ==, endtime);
+  munit_assert_uint32 (recptr->dataoffset, ==, 64);
+  munit_assert_null (recptr->next);
+
+  /* Decode data */
+  unpacked = mstl3_unpack_recordlist (id, id->first, NULL, 0, 0);
+
+  munit_assert_int64 (unpacked, ==, id->first->samplecnt);
+  munit_assert_char (id->first->sampletype, ==, 'i');
+  munit_assert_not_null (id->first->datasamples);
+  munit_assert_int64 (id->first->numsamples, ==, 3952);
+
+  int32s = (int32_t *)id->first->datasamples;
+  munit_assert_int32 (int32s[3948], ==, 28067);
+  munit_assert_int32 (int32s[3949], ==, -9565);
+  munit_assert_int32 (int32s[3950], ==, -71961);
+  munit_assert_int32 (int32s[3951], ==, -146622);
+
+  mstl3_free (&mstl, 1);
+
+  return MUNIT_OK;
+}
+
+static MunitResult
+recptr_buffer_tests (const MunitParameter params[], void *data)
+{
+  char buffer[16256];
+  FILE *fp = NULL;
+
+  MS3TraceList *mstl   = NULL;
+  MS3TraceID *id       = NULL;
+  MS3RecordPtr *recptr = NULL;
+  nstime_t endtime;
+  int64_t unpacked;
+  uint32_t flags = 0;
+  int32_t *int32s;
+  size_t rv;
+
+  char *path = "data/test-oneseries-mixedlengths-mixedorder.mseed2";
+
+  /* Silence compiler warnings about unused parameters */
+  (void) params;
+  (void) data;
+
+  /* Read test data into buffer */
+  fp = fopen (path, "rb");
+  munit_assert_not_null (fp);
+
+  rv = fread (buffer, sizeof(buffer), 1, fp);
+  munit_assert_size (rv, ==, 1);
+
+  fclose (fp);
+
+  endtime = ms_timestr2nstime ("2010-02-27T07:55:51.069539Z");
+
+  /* Set bit flag to build a record list */
+  flags |= MSF_RECORDLIST;
+
+  rv = mstl3_readbuffer (&mstl, buffer, sizeof(buffer), 0, flags, 0, 0);
+
+  munit_assert_int (rv, ==, 7);
+  munit_assert_not_null (mstl);
+  munit_assert_uint32 (mstl->numtraceids, ==, 1);
+
+  id = mstl->traces.next[0];
+
+  munit_assert_not_null (id->first);
+  munit_assert_not_null (id->first->recordlist);
+  munit_assert_int64 (id->first->samplecnt, ==, 3952);
+
+  /* No data has been decoded */
+  munit_assert_char (id->first->sampletype, ==, 0);
+  munit_assert_null (id->first->datasamples);
+  munit_assert_int64 (id->first->numsamples, ==, 0);
+
+  recptr = id->first->recordlist->last;
+  munit_assert_null (recptr->filename);      /* Record is not in a file */
+  munit_assert_not_null (recptr->bufferptr); /* Record is in a buffer */
+  munit_assert_null (recptr->fileptr);       /* File is not currently open, closed by read routine */
+  munit_assert_int64 (recptr->fileoffset, ==, 0);
+  munit_assert_not_null (recptr->msr);
+  munit_assert_int64 (recptr->endtime, ==, endtime);
+  munit_assert_uint32 (recptr->dataoffset, ==, 64);
+  munit_assert_null (recptr->next);
+
+  /* Decode data */
+  unpacked = mstl3_unpack_recordlist (id, id->first, NULL, 0, 0);
+
+  munit_assert_int64 (unpacked, ==, id->first->samplecnt);
+  munit_assert_char (id->first->sampletype, ==, 'i');
+  munit_assert_not_null (id->first->datasamples);
+  munit_assert_int64 (id->first->numsamples, ==, 3952);
+
+  int32s = (int32_t *)id->first->datasamples;
+  munit_assert_int32 (int32s[3948], ==, 28067);
+  munit_assert_int32 (int32s[3949], ==, -9565);
+  munit_assert_int32 (int32s[3950], ==, -71961);
+  munit_assert_int32 (int32s[3951], ==, -146622);
+
+  mstl3_free (&mstl, 1);
 
   return MUNIT_OK;
 }
@@ -80,6 +200,7 @@ recptr_tests (const MunitParameter params[], void *data)
 /* Create test array */
 MunitTest tracelist_tests[] = {
   { (char*) "/read", read_tests, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-  { (char*) "/recptr", recptr_tests, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/recptr/file", recptr_file_tests, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/recptr/buffer", recptr_buffer_tests, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
