c649dd4a558c1e40a6ff88711c3f9ee417ca9ed7
chad-earthscope <chad.trabant@earthscope.org>
2023-06-11 12:24:42 -0700

    Update version to v3.0.15

--- libmseed.h
+++ libmseed.h
@@ -28,8 +28,8 @@
 extern "C" {
 #endif
 
-#define LIBMSEED_VERSION "3.0.14"    //!< Library version
-#define LIBMSEED_RELEASE "2023.160"  //!< Library release date
+#define LIBMSEED_VERSION "3.0.15"    //!< Library version
+#define LIBMSEED_RELEASE "2023.162"  //!< Library release date
 
 /** @defgroup io-functions File and URL I/O */
 /** @defgroup miniseed-record Record Handling */
