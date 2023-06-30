17f4faf7da60b39d66a84ebc3d272a975271f336
chad-earthscope <chad.trabant@earthscope.org>
2023-06-29 18:34:44 -0700

    Add  qualifier to record pointer declarations

--- unpack.h
+++ unpack.h
@@ -27,13 +27,13 @@ extern "C" {
 
 #include "libmseed.h"
 
-extern int64_t msr3_unpack_mseed3 (char *record, int reclen, MS3Record **ppmsr,
+extern int64_t msr3_unpack_mseed3 (const char *record, int reclen, MS3Record **ppmsr,
                                    uint32_t flags, int8_t verbose);
-extern int64_t msr3_unpack_mseed2 (char *record, int reclen, MS3Record **ppmsr,
+extern int64_t msr3_unpack_mseed2 (const char *record, int reclen, MS3Record **ppmsr,
                                    uint32_t flags, int8_t verbose);
 
 extern double ms_nomsamprate (int factor, int multiplier);
-extern char *ms2_recordsid (char *record, char *sid, int sidlen);
+extern char *ms2_recordsid (const char *record, char *sid, int sidlen);
 extern const char *ms2_blktdesc (uint16_t blkttype);
 uint16_t ms2_blktlen (uint16_t blkttype, const char *blkt, int8_t swapflag);
 
