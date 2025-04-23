e9453b353c2a01d2d63f90fc3199d6de621096cd
chad-earthscope <chad.trabant@earthscope.org>
2025-04-23 13:40:41 -0700

    committing documentation

---
+++ search/pages_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sample_20types_0',['Sample Types',['../sample-types.html',1,'']]]
+];
---
+++ search/pages_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tutorial_0',['Tutorial',['../page-tutorial.html',1,'index']]],
+  ['types_1',['Sample Types',['../sample-types.html',1,'']]]
+];
